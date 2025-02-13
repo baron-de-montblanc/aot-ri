0e68114c233463c39654698eaca8e5e347074c9f
baron-de-montblanc <jade_ducharme@brown.edu>
2025-02-13 12:52:33 -0500

    moved api key to .env

--- src/firebase.js
+++ src/firebase.js
@@ -8,7 +8,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAEZ1-bIqhndMwtlZEPtl53bFjvfioMKc4",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "aot-ri.firebaseapp.com",
   projectId: "aot-ri",
   storageBucket: "aot-ri.firebasestorage.app",
